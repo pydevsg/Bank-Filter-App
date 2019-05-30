5091bfeb038eb65e27af37967bff0ceb47363292
Sudipto Ghosh <ghoshsudipto1129@gmail.com>
2019-05-30 11:24:29 +0530

    Update in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from "react";
 import "./App.css";
-import AutoCompleteText from "./Components/AutoCompleteText";
-import "./Components/AutoCompleteText.css";
 import DataTable from "./Components/DataTable";
-import Dropdown from "./Components/Dropdown";
-import bank from "./bank";
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="App-Component">
-          <AutoCompleteText items={bank} />
-        </div>
-        <Dropdown />
         <DataTable />
       </div>
     );
