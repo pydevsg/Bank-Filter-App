4f56536559e99892d8a9cedac16c9b56728bb721
Ankit Raj <ankit.raj928@gmail.com>
2020-01-06 12:20:02 +0530

    Add files via upload

--- src/Components/DataTable.js
+++ src/Components/DataTable.js
@@ -1,74 +1,31 @@
 import React from "react";
 import Dropdown from "./Dropdown";
-import AutoCompleteText from "./AutoCompleteText";
 import "./Datatable.css";
+import "./styles.css";
+import Table from "./Table";
+
 class DataTable extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      banks: []
-    };
-  }
+    super(props);    
+}
 
-  componentDidMount() {
-    let ct = ["BANGALORE", "CHENNAI", "DELHI", "KOLKATA", "MUMBAI", "PATNA"];
-    let url = "https://vast-shore-74260.herokuapp.com/banks?city=";
-    for (let i = 0; i < 6; i++) {
-      let curl = url + ct[i];
-      fetch(curl)
-        .then(res => res.json())
-        .then(json => {
-          this.setState({
-            isLoaded: true,
-            banks: json
-          });
-        });
-    }
-  }
-
+Pop(){
+ this.setState({
+   selected_menu:"BANGALORE"
+ })
+}
   render() {
-    const { error, isLoaded, banks } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <p>Loading...</p>;
-    } else {
+    
       return (
         <div>
-          <h1>BANK DETAILS FILTER APPLICATION</h1>
-          <Dropdown />
-          <AutoCompleteText />
-          <table class="bd">
-            <thead>
-              <tr>
-                <th>Bank IFSC</th>
-                <th>Bank Name</th>
-                <th>Bank ID</th>
-                <th>Bank Branch</th>
-                <th>Address</th>
-                <th>City</th>
-                <th>State</th>
-              </tr>
-            </thead>
-            <tbody>
-              {banks.map(bank => (
-                <tr>
-                  <td>{bank.ifsc}</td>
-                  <td>{bank.bank_name}</td>
-                  <td>{bank.bank_id}</td>
-                  <td>{bank.branch}</td>
-                  <td>{bank.address}</td>
-                  <td>{bank.city}</td>
-                  <td>{bank.state}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <div class="header"><h1>BANK DETAILS FILTER APPLICATION</h1></div>
+          
+
+          
+          <Table/>     
         </div>
       );
     }
-  }
 }
+
 export default DataTable;
